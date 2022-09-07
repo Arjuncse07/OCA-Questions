9a927867da9831558222413b26ead4113c49571d
Arjun Singh <aj629280@gmail.com>
2022-09-08 00:59:38 +0530

    "New Classes are Added 08-09-2022"

---
+++ src/com/arjun/oca2/TernaryTesrt.java
@@ -0,0 +1,10 @@
+package com.arjun.oca2;
+
+public class TernaryTesrt {
+
+   // int y=(y>100)?100:"true";
+   static int y=200;
+    public static void main(String[] args) {
+        System.out.println((y>100)?100:"true");
+    }
+}
